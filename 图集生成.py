787ed38bdf68f2f06db06531813761914dec8f73
GillEdward <1520868589@qq.com>
2023-04-10 18:23:46 +0800

    Still testing

---
+++ 图集生成.py
@@ -0,0 +1,63 @@
+import cv2
+import os
+
+def borderSlicer(img, width, height):
+	minX = width
+	minY = height
+	maxX = 0
+	maxY = 0
+
+	gap = 10
+
+	for x in range(0, width, gap):
+		for y in range(0, height, gap):
+			if img[y][x][3] != 0:
+				if x < minX:
+					minX = x
+				if y < minY:
+					minY = y
+				if x > maxX:
+					maxX = x
+				if y > maxY:
+					maxY = y
+
+	minX -= gap
+	minY -= gap
+	maxX += gap
+	maxY += gap
+	img_ = img[minY:maxY, minX:maxX]
+	#print(minX, minY, maxX, maxY)
+
+	return img_
+
+files = os.listdir('./input')
+
+for file in files:	# 预处理
+	img = cv2.imread('./input/' + file, -1)
+	height = img.shape[0]
+	width = img.shape[1]
+	depth = img.shape[2]	# 图像通道数
+
+	if depth == 4 and (img[0][0][3] == 0 or img[height - 1][width - 1][3] == 0):
+		img = borderSlicer(img, width, height)	# 消除透明底图片的多余边界
+
+	height = img.shape[0]	# 去除透明边界后的长宽
+	width = img.shape[1]
+	#print(height, width)
+
+	width = (width / height) * 1000
+	height = 1000
+
+	img = cv2.resize(img, (width, height), interpolation = cv2.INTER_AREA) 	# 将图片高度标准化到1000px
+	
+	cv2.imwrite('./input/' + file, img)
+
+'''
+1、基准高度1000px, 将5张图片转为n*1000px, 然后拼接在一起
+2、基准宽度范围6000, 若n大于6000px, 则回到1, 尝试减少1张
+3、若n小于6000px, 将连接在一起的图片宽度等比放大到6000px
+4、6列为一张，生成6000*6000px的组合图
+'''
+
+prepareImg = files[:5]	# 取五张图准备
+#for img in prepareImg:	# 
