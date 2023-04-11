119ad378c4c704e89efbb555757afb474c951e22
GillEdward <1520868589@qq.com>
2023-04-11 13:06:12 +0800

    更改阈值以处理透明图

--- 图集生成.py
+++ 图集生成.py
@@ -6,6 +6,7 @@ imgPerLine = 5
 linePerPage = 4
 BasicWidth = 6000
 BasicHeight = 1000
+transparentThreshold = 120	# 如果出现透明底图片被全部变成白色, 可以调高该阈值
 
 def borderSlicer(img, width, height):
 	minX = width
@@ -42,15 +43,19 @@ for file in files:	# 预处理
 	img = cv2.imread('./input/' + file, -1)
 	height = img.shape[0]
 	width = img.shape[1]
-	depth = img.shape[2]	# 图像通道数
+	try:
+		depth = img.shape[2]	# 图像通道数
+	except IndexError:	# 灰度图没有.shpae[2], 直接将depth赋值为3
+		depth = 3
 
 	if depth == 4:
+		print('Processing Transparent Background...')
 		img = borderSlicer(img, width, height)	# 消除透明底图片的多余边界
 		height = img.shape[0]
 		width = img.shape[1]
 		for x in range(width):	# 手动白底
 			for y in range(height):
-				if img[y][x][3] <= 50:
+				if img[y][x][3] <= transparentThreshold:
 					img[y][x][3] = 255
 					for i in range(3):
 						img[y][x][i] = 255
